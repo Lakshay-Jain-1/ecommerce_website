2fff901c519786d396cf8f1fa5d041b0a6e98d9a
Lakshay Jain <lakshaybusiness.12@gmail.com>
2024-04-11 13:23:16 +0530

    added middleware and cookie package

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cookieParser = require("cookie-parser")
 const express = require("express")
 
 const {join} =require("path")
@@ -5,13 +6,38 @@ const app = express()
 app.use(express.static('images'))
 app.use(express.static('css'))
 app.use(express.static("js"))
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+const authentication = (req,res,next)=>{
+        console.log(req.cookies)
+         if(req.cookies.loggedin){
+                res.redirect("/")
+         }else{
+                 next()
+         }
+             }
 
 app.get("/login",(req,res)=>{
     res.sendFile(join(__dirname,"login.html"))
 })
 
-app.get("/signup",(req,res)=>{
+app.post("/login",(req,res)=>{
+    const {name, user_type}=req.body;
+
+    res.cookie("username",name).cookie("user_type",user_type).cookie("loggedin",true).redirect("/")
+    
+   
+})
+
+app.get("/signup",authentication,
+    (req,res)=>{
     res.sendFile(join(__dirname,"signup.html"))
+})
+
+app.post("/signup",(req,res)=>{
+    console.log(req.body)
+    res.redirect("/")
 })
 
 app.get("/",(req,res)=>{
@@ -20,5 +46,10 @@ app.get("/",(req,res)=>{
 app.get("/cart",(req,res)=>{
     res.sendFile(join(__dirname,"cart.html"))
 })
+
+
+
+ 
+
 
 app.listen(3000,()=>{console.log("server is working")})
