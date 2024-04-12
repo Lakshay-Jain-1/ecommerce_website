91e660d12ee7147b01eeaad0577ae8f1360e4682
Lakshay Jain <lakshaybusiness.12@gmail.com>
2024-04-13 02:39:07 +0530

    added socket/websockets and made two api calls

--- index.js
+++ index.js
@@ -1,94 +1,116 @@
-const cookieParser = require("cookie-parser")
-const express = require("express")
-const cors = require("cors")
-const usermodel =require("./model/product")
-const {join} =require("path")
-const app = express()
-const multer  = require('multer')
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const cors = require("cors");
+const usermodel = require("./model/product");
+const { join } = require("path");
+const app = express();
+const multer = require("multer");
+const { Server } = require("socket.io");
+const { createServer } = require("node:http");
+
+const server = createServer(app);
+const io = new Server(server);
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      // destination
-      cb(null, "./uploads");
-    },
-    filename: function (req, file, cb) {
-      // for file name
-       
-      cb(null, file.originalname);
-    },
+  destination: function (req, file, cb) {
+    // destination
+    cb(null, "./images");
+  },
+  filename: function (req, file, cb) {
+    // for file name
+
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.static("images"));
+app.use(express.json());
+app.use(express.static("css"));
+app.use(express.static("js"));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(cors());
+
+const authentication = (req, res, next) => {
+  if (req.cookies.loggedin) {
+    res.redirect("/");
+  } else {
+    next();
+  }
+};
+
+app.get("/login", (req, res) => {
+  res.sendFile(join(__dirname, "login.html"));
+});
+
+app.post("/login", (req, res) => {
+  const { name, user_type } = req.body;
+  res
+    .cookie("username", name)
+    .cookie("user_type", user_type)
+    .cookie("loggedin", true)
+    .redirect("/");
+});
+
+app.get("/signup", authentication, (req, res) => {
+  res.sendFile(join(__dirname, "signup.html"));
+});
+
+app.post("/signup", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/", async (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+app.get("/product", async (req, res) => {
+  const data = await usermodel.find({});
+  
+  res.json(data);
+});
+
+app.delete("/delete", async (req, res) => {
+  await usermodel.findByIdAndDelete(req.query.q);
+});
+app.get("/cart", (req, res) => {
+  res.sendFile(join(__dirname, "cart.html"));
+});
+
+app.get("/vendor", (req, res) => {
+  res.sendFile(join(__dirname, "vendor.html"));
+});
+
+app.post("/test", upload.single("file"), async function (req, res) {
+  const { name, des } = req.body;
+  // req.file is the name of your file in the form above, here 'uploaded_file'
+  // req.body will hold the text fields, if there were any
+  await usermodel.create({
+    name,
+    image: `./${req.file.filename}`,
+    details: des,
   });
   
+});
+app.get("/rr",(req,res)=>{
+    res.sendFile(join(__dirname,"tet.html"))
+})
+// for socket
 
-const upload =multer({ storage });
-
-
-app.use(express.static('images'))
-app.use(express.json());
-app.use(express.static('css'))
-app.use(express.static("js"))
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
-
-app.set("view engine","ejs")
-app.use(express.static('public')); 
-app.use(cors())
-
-const authentication = (req,res,next)=>{
+io.on("connect",(socket)=>{
     
-         if(req.cookies.loggedin){
-                res.redirect("/")
-         }else{
-                 next()
-         }
-             }
-
-app.get("/login",(req,res)=>{
-  res.sendFile(join(__dirname,"login.html"))
-})
-
-app.post("/login",(req,res)=>{
-    const {name, user_type}=req.body;
-    res.cookie("username",name).cookie("user_type",user_type).cookie("loggedin",true).redirect("/")
+    socket.on("message",(text)=>{
+        console.log(text)
+        io.emit("message",text)
+    })
     
-   
 })
 
-app.get("/signup",authentication,(req,res)=>{
-    res.sendFile(join(__dirname,"signup.html"))
-})
-
-app.post("/signup",(req,res)=>{
-
-    res.redirect("/")
-})
-
-app.get("/",async(req,res)=>{
-     
-      res.sendFile(join(__dirname,"index.html"))
-})
-
-app.get("/product",async(req,res)=>{
-    const data = await usermodel.find({});
-    console.log(data)
-    res.json(data)
-})
-app.get("/cart",(req,res)=>{
-    res.sendFile(join(__dirname,"cart.html"))
-})
-
-app.get("/vendor",(req,res)=>{
-    res.sendFile(join(__dirname,"vendor.html"))
-})
-
-app.post('/test', upload.single('file'), function (req, res) {
-    // req.file is the name of your file in the form above, here 'uploaded_file'
-    // req.body will hold the text fields, if there were any 
-    console.log(req.file, req.body)
- });
-
-
-
- 
-
-
-app.listen(3000,()=>{console.log("server is working")})
+server.listen(3000, () => {
+  console.log("server is working");
+});
--- js/customer.js
+++ js/customer.js
@@ -1,3 +1,19 @@
+const socket = io();
+
+// appending data in chat box
+let text ;
+function send() {
+  const ele = document.createElement("h6");
+  ele.innerText = document.getElementById("input").value;
+  
+  document.getElementById("chatbox").appendChild(ele);
+  socket.emit("message",{msg:`${document.getElementById("input").value}`})
+  
+
+}
+
+
+
 async function fetchData() {
   try {
     const response = await fetch("http://localhost:3000/product");
@@ -39,12 +55,11 @@ function productdata(data) {
     button1.setAttribute("class", "plus");
     button1.innerText = "+";
     button1.setAttribute("value", ele.name);
-    
+
     let button2 = document.createElement("button");
     button2.setAttribute("class", "minus");
     button2.innerText = "-";
     button2.setAttribute("value", ele.name);
-   
 
     // Append elements
     sub_subparent.appendChild(button1);
@@ -62,20 +77,20 @@ function productdata(data) {
 
 // Function to add quantity
 
-
 setTimeout(() => {
   document.querySelectorAll("button.plus").forEach((ele) => {
-    
     ele.addEventListener("click", (event) => {
-      document.getElementById("asterisk").innerText = Number(document.getElementById("asterisk").innerText) + 1;
-      
-      if(localStorage.getItem(ele.value)){
-        localStorage.setItem(ele.value, Number(localStorage.getItem(ele.value))+1);
-      }else{
-        localStorage.setItem(ele.value,1);
-      }
+      document.getElementById("asterisk").innerText =
+        Number(document.getElementById("asterisk").innerText) + 1;
 
-     
+      if (localStorage.getItem(ele.value)) {
+        localStorage.setItem(
+          ele.value,
+          Number(localStorage.getItem(ele.value)) + 1
+        );
+      } else {
+        localStorage.setItem(ele.value, 1);
+      }
 
       document.getElementById("asterisk").style.display = "block";
     });
@@ -84,42 +99,30 @@ setTimeout(() => {
   document.querySelectorAll("button.minus").forEach((ele) => {
     ele.addEventListener("click", (event) => {
       const element = document.getElementById("asterisk");
-    
+
       if (Number(element.innerText) == 0) {
         document.getElementById("asterisk").style.display = "none";
         return;
       }
 
       if (localStorage.getItem(ele.value)) {
-        localStorage.setItem(ele.value, Number(localStorage.getItem(ele.value))-1);
+        localStorage.setItem(
+          ele.value,
+          Number(localStorage.getItem(ele.value)) - 1
+        );
       }
-
-     
 
       element.innerText = Number(element.innerText) - 1;
     });
   });
 }, 100);
 
+
+// for opening form 
 function openForm() {
   document.getElementById("myForm").style.display = "block";
-  
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
